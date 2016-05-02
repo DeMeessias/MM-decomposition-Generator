3913acd4261656a286d3fcd5ed8a61b3ce1d86ad
Mees van de Kerkhof <m.a.vandekerkhof@students.uu.nl>
2016-05-02 17:05:04 +0200

    Initialized Visual Studio project, created graph, vertex, and edge classes

---
+++ MMDecompositionGenerator/MMDecompositionGenerator/Data Structures/Graph.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MMDecompositionGenerator.Data_Structures
+{
+    /// <summary>
+    /// Represents a connected, simple graph
+    /// </summary>
+    class Graph
+    {
+        Dictionary<int,Vertex> vertices;
+        Dictionary<Tuple<int,int>,Edge> edges;
+
+        public int V
+        { get { return vertices.Count; } }
+        public int E
+        { get { return edges.Count; } }
+    }
+}
---
+++ MMDecompositionGenerator/MMDecompositionGenerator/Data Structures/Vertex.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MMDecompositionGenerator.Data_Structures
+{
+    class Vertex
+    {
+    }
+}
